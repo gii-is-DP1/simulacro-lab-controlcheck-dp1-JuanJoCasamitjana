8154a9b26bd11e45ea54def48a2fea6e3283dab5
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2022-09-01 12:08:18 +0200

    1-7 y 9 hechos

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,28 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
+@Entity
+public class Product extends BaseEntity {
     Integer id;
+    @NotBlank
+    @Length(min=3, max = 50)
     String name;
+    @NotNull
+    @Min(0)
     double price;
+    @ManyToOne
     ProductType productType;
 }
